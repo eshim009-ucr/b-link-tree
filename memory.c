d3ad9374e11b00dcff88287e90faf25f0e874794
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-04-16 17:19:16 -0700

    Updated mem_read_lock to be COSIM compliant

--- memory.c
+++ memory.c
@@ -10,10 +10,31 @@ Node mem_read(bptr_t address, Node const *memory) {
 	return memory[address];
 }
 
+//! The HLS interface to HBM does not support atomic test-and-set operations.
+//! Atomic test-and-set operations within the FPGA fabric are allowed.
+//! Serializing test-and-set operations in memory with mutual exclusion ensures
+//! that race conditions do not lead to unexpected concurrent modification.
+//!
+//! @todo Set up multiple locks for specific regions of memory, such as by
+//! address ranges or hashes to allow higher write bandwidth.
 Node mem_read_lock(bptr_t address, Node *memory) {
+	static lock_t local_readlock = 0;
+	Node tmp;
+
 	assert(address < MEM_SIZE);
-	lock_p(&memory[address].lock);
-	return mem_read(address, memory);
+	lock_p(&local_readlock);
+	// Read the given address from main memory until its lock is released
+	do {
+		tmp = memory[address];
+	} while(lock_test(&tmp.lock));
+	// After the lock is released, we can safely acquire it because no other
+	// modules concurrently hold this function's lock
+	lock_p(&tmp.lock);
+	// Write back the locked value to main memory
+	memory[address] = tmp;
+	// Release the local lock for future writers
+	lock_v(&local_readlock);
+	return tmp;
 }
 
 void mem_write_unlock(AddrNode *node, Node *memory) {
