422c8ce7288d27ac76181fe2334837c15497d364
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-06-03 18:30:35 -0700

    Single-Threaded test suite

--- batch-tests.sh
+++ batch-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -e
 
 
-RERUNS=1
+BATCHES=4
+RERUNS=4
 
 
 echo 'Generating Testing Binary...'
@@ -14,95 +16,26 @@ make clean all
 ./gen-bench
 
 
-echo '=== INSERT PARALLELISM ==='
-echo '--- 1 Thread ---'
-for i in `seq $RERUNS`; do
-	echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_req.bin
-done
-echo '--- 2 Threads ---'
-for i in `seq $RERUNS`; do
-	echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_1-of-2_req.bin insert_random_2-of-2_req.bin
-done
-echo '--- 4 Threads ---'
-for i in `seq $RERUNS`; do
-	echo " -  Run $i/$RERUNS  - "
-	../blink exe \
-		insert_random_1-of-4_req.bin insert_random_2-of-4_req.bin \
-		insert_random_3-of-4_req.bin insert_random_4-of-4_req.bin
-done
-echo '--- 8 Threads ---'
-for i in `seq $RERUNS`; do
-	 echo " -  Run $i/$RERUNS  - "
-	../blink exe \
-		insert_random_1-of-8_req.bin insert_random_2-of-8_req.bin \
-		insert_random_3-of-8_req.bin insert_random_4-of-8_req.bin \
-		insert_random_5-of-8_req.bin insert_random_6-of-8_req.bin \
-		insert_random_7-of-8_req.bin insert_random_8-of-8_req.bin
-done
-
-
-echo '=== SEARCH PARALLELISM ==='
-echo '--- 1 Thread ---'
-for i in `seq $RERUNS`; do
-	echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_req.bin then search_random_req.bin
-done
-echo '--- 2 Threads ---'
-for i in `seq $RERUNS`; do
-	echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_req.bin then \
-		search_random_1-of-2_req.bin search_random_2-of-2_req.bin
-done
-echo '--- 4 Threads ---'
-for i in `seq $RERUNS`; do
-	echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_req.bin then \
-		search_random_1-of-4_req.bin search_random_2-of-4_req.bin \
-		search_random_3-of-4_req.bin search_random_4-of-4_req.bin
-done
-echo '--- 8 Threads ---'
-for i in `seq $RERUNS`; do
-	 echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_req.bin then \
-		search_random_1-of-8_req.bin search_random_2-of-8_req.bin \
-		search_random_3-of-8_req.bin search_random_4-of-8_req.bin \
-		search_random_5-of-8_req.bin search_random_6-of-8_req.bin \
-		search_random_7-of-8_req.bin search_random_8-of-8_req.bin
+echo '=== INSERT & SEARCH ==='
+for i in `seq $BATCHES`; do
+	echo "--- Batch $i/$BATCHES ---"
+	for j in `seq $RERUNS`; do
+		echo " -  Run $j/$RERUNS  - "
+		../blink exe "insert_random_$i-of-$(echo $BATCHES)_req.bin" then \
+			"search_random_$i-of-$(echo $BATCHES)_req.bin"
+	done
 done
 
 
 echo '=== READ/WRITE DOMINANCE ==='
-echo '--- 20% Read, 80% Write ---'
-for i in `seq $RERUNS`; do
-	 echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_1-of-4_req.bin then \
-		search_random_1-of-5_req.bin insert_random_2-of-5_req.bin \
-		insert_random_3-of-5_req.bin insert_random_4-of-5_req.bin \
-		insert_random_5-of-5_req.bin
-done
-echo '--- 40% Read, 60% Write ---'
-for i in `seq $RERUNS`; do
-	 echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_1-of-4_req.bin then \
-		search_random_1-of-5_req.bin search_random_2-of-5_req.bin \
-		insert_random_3-of-5_req.bin insert_random_4-of-5_req.bin \
-		insert_random_5-of-5_req.bin
-done
-echo '--- 60% Read, 40% Write ---'
-for i in `seq $RERUNS`; do
-	 echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_1-of-4_req.bin then \
-		search_random_1-of-5_req.bin search_random_2-of-5_req.bin \
-		search_random_3-of-5_req.bin insert_random_4-of-5_req.bin \
-		insert_random_5-of-5_req.bin
-done
-echo '--- 80% Read, 20% Write ---'
-for i in `seq $RERUNS`; do
-	 echo " -  Run $i/$RERUNS  - "
-	../blink exe insert_random_1-of-4_req.bin then \
-		search_random_1-of-5_req.bin search_random_2-of-5_req.bin \
-		search_random_3-of-5_req.bin search_random_4-of-5_req.bin \
-		insert_random_5-of-5_req.bin
+for r_ratio in $(seq 20 20 80); do
+	w_ratio=$((100 - $r_ratio))
+	echo "--- $r_ratio% Read, $w_ratio% Write ---"
+	for i in `seq $BATCHES`; do
+		for j in `seq $RERUNS`; do
+			echo " -  Batch $i/$BATCHES, Run $j/$RERUNS  - "
+			../blink exe "insert_random_$i-of-$(echo $BATCHES)_req.bin" then \
+				"mixed-rw_$r_ratio-$(echo $w_ratio)_$i-of-$(echo $BATCHES)_req.bin"
+		done
+	done
 done
