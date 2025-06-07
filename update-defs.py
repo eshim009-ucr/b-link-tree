296acba23ee2fe5ee56bc178b5fef33358c5041c
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-06-06 19:26:57 -0700

    Copied over update-defs fix from lyao branch

--- update-defs.py
+++ update-defs.py
@@ -18,16 +18,20 @@ f"""Usage:
 		print(f"entry_count={ENTRY_COUNT}")
 	else:
 		ENTRY_COUNT = TREE_ORDER * 10
-	MIN_LEAVES = ceil(ENTRY_COUNT/TREE_ORDER)
-	print(f"necessary_leaves={MIN_LEAVES}")
-	MAX_NODES_PER_LEVEL = TREE_ORDER ** ceil(log(MIN_LEAVES, TREE_ORDER))
-	print(f"MAX_NODES_PER_LEVEL={MAX_NODES_PER_LEVEL}")
-	MAX_LEVELS = 1 + ceil(log(MAX_NODES_PER_LEVEL, TREE_ORDER))
+	ENTRY_MAX = TREE_ORDER
+	MAX_NODES_PER_LEVEL = 1
+	MAX_LEVELS = 1
+	while ENTRY_MAX < ENTRY_COUNT:
+		MAX_NODES_PER_LEVEL = ENTRY_MAX
+		ENTRY_MAX = (ENTRY_MAX+1) * (TREE_ORDER//2)
+		MAX_LEVELS += 1
+	MAX_LEAVES = MAX_NODES_PER_LEVEL
 	print(f"MAX_LEVELS={MAX_LEVELS}")
+	print(f"MAX_LEAVES={MAX_LEAVES}")
+	print(f"MAX_NODES_PER_LEVEL=MAX_LEAVES")
 	MEM_SIZE = MAX_NODES_PER_LEVEL * MAX_LEVELS
 	print(f"MEM_SIZE={MEM_SIZE}")
-	MEM_SIZE = "(MAX_NODES_PER_LEVEL * MAX_LEVELS)"
-	MAX_LEAVES = "MAX_NODES_PER_LEVEL"
+	print(f"ENTRY_MAX={ENTRY_MAX}")
 
 	# Update file in place
 	print("Updating file...", end="")
@@ -36,11 +40,13 @@ f"""Usage:
 	defs_h_str = sub(r"#\s*define\s+MEM_SIZE\s+.+",
 		f"#define MEM_SIZE {MEM_SIZE}", defs_h_str)
 	defs_h_str = sub(r"#\s*define\s+MAX_LEVELS\s+.+",
-		f"#define MAX_LEVELS {MAX_LEVELS}", defs_h_str)
+		f"#define MAX_LEVELS ((bptr_t) {MAX_LEVELS})", defs_h_str)
 	defs_h_str = sub(r"#\s*define\s+MAX_NODES_PER_LEVEL\s+.+",
-		f"#define MAX_NODES_PER_LEVEL {MAX_NODES_PER_LEVEL}", defs_h_str)
+		f"#define MAX_NODES_PER_LEVEL ((bptr_t) {MAX_NODES_PER_LEVEL})", defs_h_str)
 	defs_h_str = sub(r"#\s*define\s+MAX_LEAVES\s+.+",
-		f"#define MAX_LEAVES {MAX_LEAVES}", defs_h_str)
+		f"#define MAX_LEAVES ((bptr_t) {MAX_LEAVES})", defs_h_str)
+	defs_h_str = sub(r"#\s*define\s+ENTRY_MAX\s+.+",
+		f"#define ENTRY_MAX {ENTRY_MAX}UL", defs_h_str)
 	with open("defs.h", "w") as fout:
 		fout.write(defs_h_str)
 	print("Done!")
