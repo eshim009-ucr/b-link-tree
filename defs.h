6688e33b2f44b40dbc0a1b82434111c014f2f234
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-06-07 13:20:04 -0700

    Added missing fields used by update-defs script

--- defs.h
+++ defs.h
@@ -8,23 +8,31 @@
 //! "Width" of a memory grid
 //! Each level of the tree is on its own level
 #ifndef MAX_NODES_PER_LEVEL
-#define MAX_NODES_PER_LEVEL (16)
+#define MAX_NODES_PER_LEVEL ((bptr_t) 46)
 #endif
 //! Maximum height of a tree
 #ifndef MAX_LEVELS
-#define MAX_LEVELS (3)
+#define MAX_LEVELS ((bptr_t) 3)
 #endif
 //! Maximum number of leaf nodes in the entire tree
 #ifndef MAX_LEAVES
-#define MAX_LEAVES MAX_NODES_PER_LEVEL
+#define MAX_LEAVES ((bptr_t) 46)
 #endif
 //! Maximum number of nodes that can be stored in a tree's memory
 #ifndef MEM_SIZE
-#define MEM_SIZE (MAX_NODES_PER_LEVEL * MAX_LEVELS)
+#define MEM_SIZE 51ULL
 #endif
 //! Maximum number of entries that can be safely inserted
 #ifndef ENTRY_MAX
-#define ENTRY_MAX ((TREE_ORDER/2)+(MAX_LEAVES/2))
+#define ENTRY_MAX 22ULL
+#endif
+
+#ifndef LEVEL_STARTS
+#define LEVEL_STARTS {0, 46, 50, 51}
+#endif
+
+#ifndef LEVEL_WIDTHS
+#define LEVEL_WIDTHS {46, 4, 1}
 #endif
 
 #if !defined NO_GTEST && !__has_include("gtest/gtest.h")
