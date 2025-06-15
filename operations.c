711812cc7413f6d34d2c6e77b278297f8bb3890c
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2025-06-14 19:28:13 -0700

    Removed functions that are broken under HLS

--- operations.c
+++ operations.c
@@ -4,39 +4,6 @@
 #include "insert.h"
 
 
-Request encode_search_req(search_in_t in) {
-	Request req = {
-		.opcode = SEARCH,
-		{ .search = in }
-	};
-	return req;
-}
-
-Request encode_insert_req(insert_in_t in) {
-	Request req = {
-		.opcode = INSERT,
-		{ .insert = in }
-	};
-	return req;
-}
-
-Response encode_search_resp(search_out_t out) {
-	Response resp = {
-		.opcode = SEARCH,
-		{ .search = out }
-	};
-	return resp;
-}
-
-Response encode_insert_resp(insert_out_t out) {
-	Response resp = {
-		.opcode = INSERT,
-		{ .insert = out }
-	};
-	return resp;
-}
-
-
 Response execute_req(Request req, bptr_t *root, Node *memory) {
 	Response resp = {.opcode = req.opcode};
 	switch (req.opcode) {
